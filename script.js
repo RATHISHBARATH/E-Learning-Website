496928982b728f0b399543a5aa89b1f9632eddae
RATHISHBARATH <94107495+RATHISHBARATH@users.noreply.github.com>
2024-07-04 17:33:52 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+  <script>
+        // Example JavaScript for button click handling
+        document.querySelector('.Getforfree').addEventListener('click', function() {
+            alert('Button clicked!'); // Replace with your desired functionality
+        });
+    </script>
